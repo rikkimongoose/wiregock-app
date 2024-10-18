ec0de73ef0ae374fb9b83d29ec698d3b5de2a30e
Rikki Mongoose <alexander.a.teut@gmail.com>
2024-10-19 00:08:22 +0300

    add basicAuth

--- wiregock.go
+++ wiregock.go
@@ -12,6 +12,8 @@ import (
     "io/ioutil"
     "crypto/tls"
     "crypto/x509"
+    "crypto/subtle"
+    "crypto/sha256"
     "github.com/ilyakaznacheev/cleanenv"
     "go.uber.org/zap"
     "go.uber.org/zap/zapcore"
@@ -111,6 +113,9 @@ func main() {
         for _, mock := range mockRoute.mocks {
             methods := wiregock.LoadMethods(*mock.Request.Method)
             handler := generateHandler(&mock)
+            if mock.Request.BasicAuthCredentials != nil && mock.Request.BasicAuthCredentials.Username != nil && mock.Request.BasicAuthCredentials.Password != nil {
+                handler = basicAuth(handler, *mock.Request.BasicAuthCredentials.Username, *mock.Request.BasicAuthCredentials.Password)
+            }
             var url string
             if mock.Request.UrlPath != nil {
                 url = *mock.Request.UrlPath
@@ -295,6 +300,48 @@ func generateHandler(mock *wiregock.MockData) http.HandlerFunc {
         }
         
     }
+}
+func basicAuth(next http.HandlerFunc, username string, password string) http.HandlerFunc {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // Extract the username and password from the request 
+        // Authorization header. If no Authentication header is present 
+        // or the header value is invalid, then the 'ok' return value 
+        // will be false.
+        username, password, ok := r.BasicAuth()
+        if ok {
+            // Calculate SHA-256 hashes for the provided and expected
+            // usernames and passwords.
+            usernameHash := sha256.Sum256([]byte(username))
+            passwordHash := sha256.Sum256([]byte(password))
+            expectedUsernameHash := sha256.Sum256([]byte(username))
+            expectedPasswordHash := sha256.Sum256([]byte(password))
+
+            // Use the subtle.ConstantTimeCompare() function to check if 
+            // the provided username and password hashes equal the  
+            // expected username and password hashes. ConstantTimeCompare
+            // will return 1 if the values are equal, or 0 otherwise. 
+            // Importantly, we should to do the work to evaluate both the 
+            // username and password before checking the return values to 
+            // avoid leaking information.
+            usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+            passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+            // If the username and password are correct, then call
+            // the next handler in the chain. Make sure to return 
+            // afterwards, so that none of the code below is run.
+            if usernameMatch && passwordMatch {
+                next.ServeHTTP(w, r)
+                return
+            }
+        }
+
+        // If the Authentication header is not present, is invalid, or the
+        // username or password is wrong, then set a WWW-Authenticate 
+        // header to inform the client that we expect them to use basic
+        // authentication and send a 401 Unauthorized response.
+        w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+    })
 }
 
 func mongoTlsConfig(caFile string, certFile string, keyFile string) *tls.Config {
