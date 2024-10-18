6f806383e7a597be73d4b5b9f6a9324d93dc8742
Rikki Mongoose <alexander.a.teut@gmail.com>
2024-10-18 23:57:38 +0300

    premade server

--- wiregock.go
+++ wiregock.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+    "io"
     "fmt"
     "flag"
+    "os"
+    "strings"
+    "context"
+    "time"
+    "net/http"
+    "io/ioutil"
+    "crypto/tls"
+    "crypto/x509"
     "github.com/ilyakaznacheev/cleanenv"
     "go.uber.org/zap"
     "go.uber.org/zap/zapcore"
-
-    "context"
-    "crypto/tls"
-    "crypto/x509"
-    "os"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
+    "github.com/gorilla/mux"
     "github.com/rikkimongoose/wiregock"
-    "strings"
+    actuator "github.com/sinhashubham95/go-actuator"
 )
 
 const (
@@ -55,7 +60,6 @@ var log *zap.Logger
 var config AppConfig
 
 func main() {
-
     var err error
     cfgPath := *flag.String("CONFIG", "config.yml", "Path to application config file")
     err = cleanenv.ReadConfig(cfgPath, &config)
@@ -79,46 +83,90 @@ func main() {
     log = zap.Must(zc.Build())
     
     defer log.Sync() // все асинхронные логи будут записаны перед выходом
-    mockRoutesData := loadMocksFromMongo(config.Mongo.Collection)
-    if mockRoutesData == nil {
-        log.Warn(`Mocks collection is empty`)
+
+    router := mux.NewRouter()
+    actuatorConfig := &actuator.Config{
+        Endpoints: []int{
+            actuator.Env,
+            actuator.Info,
+            actuator.Metrics,
+            actuator.Ping,
+            actuator.Shutdown,
+            actuator.ThreadDump,
+        },
+        Name: productName,
+        Port: config.Server.Port,
+        Version: productVersion,
     }
-    /*else {
-        for _, mockRoute := range mockRoutes {
-        for _, mock := mockRoute.mocks {
-            installMock(&server, &mock)
+    actuatorHandler := actuator.GetActuatorHandler(actuatorConfig)
+    router.PathPrefix("/actuator/").Handler(actuatorHandler)
+    mockRoutesData := loadMocksFromMongo(
+        config.Mongo.Url,
+        config.Mongo.Database,
+        config.Mongo.Collection,
+        config.Mongo.CaFile,
+        config.Mongo.CertFile,
+        config.Mongo.KeyFile)
+    for _, mockRoute := range mockRoutesData.mockRoutes {
+        for _, mock := range mockRoute.mocks {
+            methods := wiregock.LoadMethods(*mock.Request.Method)
+            handler := generateHandler(&mock)
+            var url string
+            if mock.Request.UrlPath != nil {
+                url = *mock.Request.UrlPath
+            } else if mock.Request.UrlPattern != nil {
+                url = *mock.Request.UrlPattern
+            } else {
+                log.Warn(`No url defined for mock`)
+                continue
+            }
+
+            router.PathPrefix(url).Handler(handler).Methods(methods...)
         }
     }
-}*/
+    serverAddr := fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)
+    log.Info("Starting up", zap.String("server", serverAddr))
+    srv := &http.Server{
+        Handler:      router,
+        Addr:         serverAddr,
+        // Good practice: enforce timeouts for servers you create!
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
+    err = srv.ListenAndServe()
+    if err != nil {
+       log.Error(`Error starting server`,
+            zap.Error(err),
+            zap.String("host", config.Server.Host),
+            zap.Int("port", config.Server.Port))
+    }
 }
 
-
-func loadMocksFromMongo(storages []string) *MockRoutesData {
+func loadMocksFromMongo(url string, db string, storages []string, caFile string, certFile string, keyFile string) *MockRoutesData {
     ctx := context.TODO()
-    tlsConfig := mongoTlsConfig(config.Mongo.CaFile, config.Mongo.CertFile, config.Mongo.KeyFile)
     opts := options.Client().ApplyURI(config.Mongo.Url)
+    tlsConfig := mongoTlsConfig(caFile, certFile, keyFile)
     if tlsConfig != nil {
         opts = opts.SetTLSConfig(tlsConfig)
     }
 
     client, err := mongo.Connect(ctx, opts)
     if err != nil {
-        log.Error(`Database connection error`, zap.Error(err), zap.String("db", config.Mongo.Url))
+        log.Error(`Database connection error`, zap.Error(err), zap.String("db", url))
         return nil
     }
 
     var resultPing bson.M
     if err := client.Database(config.Mongo.Database).RunCommand(ctx, bson.D{{"ping", 1}}).Decode(&resultPing); err != nil {
-        log.Error(`Database ping error`, zap.Error(err), zap.String("db", config.Mongo.Database))
+        log.Error(`Database ping error`, zap.Error(err), zap.String("db", db))
         return nil
     }
 
     log.Info("Pinged your deployment. You successfully connected to MongoDB!")
 
     var mockRoutes []MockRoute
-
     for _, storage := range storages {
-        mockRoute := loadMock(client, &ctx, storage)
+        mockRoute := loadMock(db, client, &ctx, storage)
         if mockRoute != nil {
             mockRoutes = append(mockRoutes, *mockRoute)
         }
@@ -127,14 +175,14 @@ func loadMocksFromMongo(storages []string) *MockRoutesData {
     return &MockRoutesData{mockRoutes}
 }
 
-func loadMock(client *mongo.Client, ctx *context.Context, mockSource string) *MockRoute {
+func loadMock(db string, client *mongo.Client, ctx *context.Context, mockSource string) *MockRoute {
     var mocks []wiregock.MockData
-    wiremockCollection := client.Database(config.Mongo.Database).Collection(mockSource)
-    cursor, err := wiremockCollection.Find(*ctx, bson.M{})
+    wiregockCollection := client.Database(db).Collection(mockSource)
+    cursor, err := wiregockCollection.Find(*ctx, bson.M{})
     if err != nil {
         log.Error(`Database cursor creation error`,
             zap.Error(err),
-            zap.String("db", config.Mongo.Url),
+            zap.String("db", db),
             zap.String("collection", mockSource))
         return nil
     }
@@ -151,97 +199,103 @@ func loadMock(client *mongo.Client, ctx *context.Context, mockSource string) *Mo
         } else {
             log.Info(`Rule loaded, but no URL to attach`)
         }
-        
         mocks = append(mocks, mock)
     }
     return &MockRoute{mocks}
 }
 
-/*func installWiremock(server *fiber.App, mock *wiremock.MockData) {
-    if mock.Request == nil {
-        return
-    }
-    request := mock.Request
-    if request.Method == nil {
-        return
-    }
-    methodNames := loadMethods(*request.Method)
-    if request.BasicAuthCredentials != nil && request.BasicAuthCredentials.Username != nil && request.BasicAuthCredentials.Password != nil {
-        server.Use(basicauth.New(basicauth.Config{
-            Users: map[string]string{
-                *request.BasicAuthCredentials.Username: *request.BasicAuthCredentials.Password
-                },
-            }))
-    }
-
-    url := request.UrlPath
-    if url == nil {
-        url = &fmt.Sprintf("regex(%s)", *request.UrlPattern)
-    }
-
-    server.Add(methodNames, *url, func(c fiber.Ctx) error {
-        dc := DataContext{
+func generateHandler(mock *wiregock.MockData) http.HandlerFunc {
+    return func(w http.ResponseWriter, req *http.Request) {
+        dc := wiregock.DataContext{
             Body: func() string {
-                return string(c.Body()[:])
-            }
+                b, err := ioutil.ReadAll(req.Body)
+                if err != nil {
+                    return ""
+                }
+                return string(b[:])
+            },
             Get: func(key string) string {
-                return c.Get(key, "")
-            }
+                return req.Header.Get(key)
+            },
             Params: func(key string) string {
-                return c.Params(key, "")
-            }
+                return req.URL.Query().Get(key)
+            },
             Cookies: func(key string) string {
-                return c.Cookies(key, "")
-            }
+                cookie, err := req.Cookie(key)
+                if err != nil {
+                    return ""
+                }
+                return cookie.Value
+            },
         }
-        condition := parseCondition(request, &dc)
-        result, err := condition.check()
+        flusher, ok := w.(http.Flusher)
+        if !ok {
+            http.NotFound(w, req)
+            return
+        }
+        condition, err := wiregock.ParseCondition(mock.Request, &dc)
         if err != nil {
-           return c.Status(fiber.StatusInternalServerError).SendString(err)
+            http.Error(w, "Wrong condition", http.StatusInternalServerError)
+            return
+        }
+        result, err := condition.Check()
+        if err != nil {
+            http.Error(w, "Error with rule execution", http.StatusInternalServerError)
+            return
         }
         if !result {
-            return c.Status(fiber.StatusNotFound)
+            w.WriteHeader(http.StatusNotAcceptable)
+            return
         }
 
-        traceId := c.Params("traceparent")
-        if traceId == nil {
-            c.Set("traceparent", generateTraceparent())
+        traceId := req.Header.Get("traceparent")
+        if strings.Compare(traceId, "") == 0 {
+            w.Header().Set("traceparent", wiregock.GenerateTraceparent())
         } else {
-            c.Set("traceparent", traceId)
+            w.Header().Set("traceparent", traceId)
         }
 
-        if mock.Response == nil {
-            return c.Status(fiber.StatusOK).SendString("")
-        }
-
+        statusCode := http.StatusOK
         response := mock.Response
+        if response.Status != nil {
+            statusCode = *response.Status
+        }
+        w.WriteHeader(statusCode)
+        if response == nil {
+            return
+        }
+
         if response.Headers != nil {
             for key, value := range response.Headers {
-                c.Set(key, value)
+                w.Header().Set(key, value)
             }
         }
 
         if response.Cookies != nil {
             for key, value := range response.Cookies {
-                c.Cookie(key, value)
+                cookie := http.Cookie{
+                    Name:     key,
+                    Value:    value,
+                    Path:     "/",
+                    MaxAge:   3600,
+                    HttpOnly: true,
+                    Secure:   true,
+                    SameSite: http.SameSiteLaxMode,
+                }
+                http.SetCookie(w, &cookie)
             }
         }
 
-        if statusCode != nil {
-            c.Status(*response.Status)
-        } else {
-            c.Status(fiber.StatusOK)
+        if statusCode != http.StatusOK {
+            w.WriteHeader(statusCode)
+        }
+        if response.Body != nil {
+            flusher.Flush()
+            io.WriteString(w, *response.Body)
         }
         
-
-        body := response.Body
-        if body != nil {
-            return c.SendString(*body)
-        }
-        return nil
-        
-    })
-}*/
+    }
+}
 
 func mongoTlsConfig(caFile string, certFile string, keyFile string) *tls.Config {
     caCertPool := x509.NewCertPool()
@@ -274,4 +328,3 @@ func mongoTlsConfig(caFile string, certFile string, keyFile string) *tls.Config 
         Certificates: []tls.Certificate{cert},
     }
 }
-
